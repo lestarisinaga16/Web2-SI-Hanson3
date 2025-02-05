1b0468aff61f6b20b2cf19b2bf3dbc068a0c80ad
lestarisinaga16 <tarisrisinaga@gmail.com>
2025-02-05 15:58:51 +0700

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <table>
+      <tr>
+        <th>NIM</th>
+        <th>NAMA</th>
+        <th>Jurusan</th>
+        <th>Email</th>
+        <th>Hobby</th>
+        </tr>
+        <tr>
+          <td>2382024</td>
+          <td>Lestari Sinaga</td>
+          <td>Sistem Informasi</td>
+          <td>tarisrisinaga@gmail.com</td>
+          <td>Bernyanyi</td>
+        </tr>
+        </table>
+    </>
+  )
+}
+
+export default App
